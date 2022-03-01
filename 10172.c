2fa913fc33870364575db0fdc8ea09bce96f40dc
zzsla <kangbs21026@naver.com>
2022-03-01 17:50:54 +0900

    개ver0

---
+++ 10172.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("|\\_/| ");
+	printf("\n");
+	printf("|q p|   /} ");
+	printf("\n");
+	printf("( 0 )\"\"\"\\ ");
+	printf("\n");
+	printf("|\"^\"'    | ");
+	printf("\n");
+	printf("||_/=\\\\__| ");
+
+	return 0;
+}
