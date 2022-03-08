976b590f46d78a9fb2b100a67dd3bb7f661e9d7b
zzsla <kangbs21026@naver.com>
2022-03-09 01:24:18 +0900

    평균은 넘겠지ver0.2

--- 4344.c
+++ 4344.c
@@ -1,42 +1,30 @@
 #include <stdio.h>
-int in_valve(int* n);
 
 int main(void)
 {
 	int C;
 	scanf("%i", &C);
-	getchar();
 	for(int i = 0; i < C; i++)
 	{
-		int n[1004] = {};
+		int a;
+		scanf("%i", &a);
+		int n[1000] = {};
+		for(int b = 0; b < a; b++) scanf("%i", &n[b]);
 		int count = 0;
 		float tmp = 0;
 		float tmp2 = 0;
 		int nu = 0;
-		char p = '%';
-		count = in_valve(n);
-		for(int j = 1; j < count; j++)
+		count = a;
+		for(int j = 0; j < count; j++)
 			tmp += n[j];
-		tmp /= n[0];
-		for(int j = 1; j < count; j++)
+		tmp /= a;
+		for(int j = 0; j < count; j++)
 			if(tmp < n[j]) nu++;
 		tmp2 = nu *100;
-		tmp = tmp2 / n[0];
-		printf("%6.3f", tmp);
-		printf("%c",p);
+		tmp = tmp2 / a;
+		printf("%0.3f%%", tmp);
 		printf("\n");
 
 	}
 	return 0;
-}
-
-int in_valve(int* n)
-{
-	int i;
-	for(i = 0; i < 1000; i++)
-	{
-		scanf("%i",&n[i]);
-		if(getchar() == '\n') break;
-	}
-	return i+1;
 }
