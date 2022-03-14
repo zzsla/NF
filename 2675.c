bc8118c7568a9a281c5aa75adbfa8e58201ca33b
zzsla <kangbs21026@naver.com>
2022-03-14 12:59:07 +0900

    문자열ver0.1

--- 2675.c
+++ 2675.c
@@ -4,10 +4,10 @@ int main(void)
 {
 	int T;
 	scanf("%i", &T);
-	char P[T][161];
 	if(!(1 <= T && T <= 1000)) return 1;
 	for(int i = 0; i < T; i++)
 	{
+		char P[161] = {};
 		int R = 0;
 		scanf("%i", &R);
 		if(!(1 <= R && R <= 8)) return 1;
@@ -18,21 +18,12 @@ int main(void)
 		{
 			for(int j = 0; j < R; j++)
 			{
-				P[i][p] = tmp[k];
+				P[p] = tmp[k];
 				p++;
 			}
 			k++;
 		}
-	}
-	for(int i = 0; i < T; i++)
-	{
-		int p = 0;
-		while(P[i][p] != '\0')
-		{
-			printf("%c", P[i][p]);
-			p++;
-		}
-		printf("\n");
+		printf("%s\n",&P[0]);
 	}
 	return 0;
 }
