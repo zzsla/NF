771e3e823d1c41aaade81decfd81f3c0db1cf336
zzsla <kangbs21026@naver.com>
2022-03-15 23:03:51 +0900

    벌집ver0

---
+++ 2292.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int N;
+	int room = 1;
+	int i = 0;
+	scanf("%i",&N);
+	if(!(1 <= N && N <= 1000000000)) return 1;
+
+	while(room <= N)
+	{
+		i++;
+		room = room + (6 * i);
+	}
+	printf("%i", i + 1);
+
+	return 0;
+}
