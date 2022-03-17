4df98def391dc2f846507c2c6710245e59833df5
zzsla <kangbs21026@naver.com>
2022-03-17 21:54:31 +0900

    소수ver0

---
+++ 2581.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int M, N;
+	scanf("%i %i", &M, &N);
+	int tmp;
+	int result = 0, min;
+	while(M <= N)
+	{
+		for(int i = 2; i < M; i++)
+		{
+			tmp = M % i;
+			if(tmp == 0) break;
+		}
+		if(tmp != 0)
+		{
+			if(result == 0) min = M;
+			result += M;
+		}
+		M++;
+	}
+	if(result == 0) printf("-1");
+	else
+	{
+		printf("%i\n%i", result, min);
+	}
+
+	return 0;
+}
