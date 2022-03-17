30ed8205d1740d158d848c45a00ef665987aee76
zzsla <kangbs21026@naver.com>
2022-03-18 00:35:40 +0900

    소수 구하기ver0.1

--- 1929.c
+++ 1929.c
@@ -1,22 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
+	char n[1000000] = {};
+	char *arr;
+	arr = (char *)malloc(sizeof(char) * 1000000);
+	arr[0] = 1;
+	arr[1] = 1;
+	for(int i = 2; i < 1000001; )
+	{
+		int j = 2;
+		while((i*j) < 1000001)
+		{
+			arr[i*j] = 1;
+			j++;
+		}
+		i++;
+		if(arr[i] == 1)
+			while(arr[i] != 0)i++;
+	}
 	int M, N;
 	scanf("%i %i", &M, &N);
 	if(M < 1 || N < 1 || M > 1000000 || N > 1000000) return 1;
-	if(M == 1) M = 2;
-	int div, rem, tmp;
-	for(int i = M; i <= N; i++)
+	
+	while(M <= N)
 	{
-		int j;
-		for(j = 2; j < i; j++)
-		{
-			rem = i % j;
-			if(rem == 0) break;
-		}
-		if(j == i) printf("%i\n",i);
+		if(arr[M] == 0) printf("%i\n", M);
+		M++;
 	}
+	free(arr);
 
 	return 0;
 }
