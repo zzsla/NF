8ad6eb431468adf444468c8eebfe65291702727c
zzsla <kangbs21026@naver.com>
2022-03-24 19:03:24 +0900

    수 정렬하기 3ver0

---
+++ 10989.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void) //counting sort
+{
+	int N;
+	scanf("%i", &N);
+	if(!(1 <= N && N <= 10000000)) return 1;
+	int n[N];
+	int result[N + 1];
+	int cntnum[10000] = {};
+	int tmp;
+	for(int i = 0; i < N; i++)
+		scanf("%i", &n[i]);
+	for(int i = 0; i < N; i++){
+		tmp = n[i];
+		cntnum[tmp]++;
+	}
+	for(int i = 1; i < 10000; i++)
+		cntnum[i] += cntnum[i-1];
+	for(int i = N -1; i >= 0; i--){
+		tmp = n[i];
+		result[cntnum[tmp]] = tmp;
+		cntnum[tmp]--;
+	}
+	for(int i = 1; i <= N; i++)
+		printf("%i\n",result[i]);
+
+	return 0;
+}
