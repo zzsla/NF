fd7ff8391bfd1673bbc3a8f7b446dbeb28dfc02b
zzsla <kangbs21026@naver.com>
2022-03-24 21:02:20 +0900

    수 정렬하기 3ver0.3

--- 10989.c
+++ 10989.c
@@ -7,27 +7,32 @@ int main(void) //counting sort
 	scanf("%i", &N);
 	if(!(1 <= N && N <= 10000000)) return 1;
 	int *n;
-	n = (int *)malloc(sizeof(int *) * N);
-	int *result;
-	result = (int *)malloc(sizeof(int *) * N + 1);
+	n = (int *)malloc(sizeof(int *) * N + 1);
 	int *cntnum;
 	cntnum = (int *)malloc(sizeof(int *) * 10000);
-	int tmp;
+	int tmp, tmp2;
 	for(int i = 0; i < N; i++)
 		scanf("%i", &n[i]);
 	for(int i = 0; i < N; i++){
 		tmp = n[i];
 		cntnum[tmp]++;
 	}
+	cntnum[1] += -1;
 	for(int i = 1; i < 10000; i++)
 		cntnum[i] += cntnum[i-1];
-	for(int i = N -1; i >= 0; i--){
-		tmp = n[i];
-		result[cntnum[tmp]] = tmp;
+	int i = N - 1;
+	tmp = n[cntnum[n[0]]];
+	n[cntnum[n[0]]] = n[0];
+	cntnum[n[0]]--;
+	while(i != 0){
+		tmp2 = n[cntnum[tmp]];
+		n[cntnum[tmp]] = tmp;
 		cntnum[tmp]--;
+		i--;
+		tmp = tmp2;
 	}
-	for(int i = 1; i <= N; i++)
-		printf("%i\n",result[i]);
+	for(int i = 0; i < N; i++)
+		printf("%i\n",n[i]);
 
 	return 0;
 }
