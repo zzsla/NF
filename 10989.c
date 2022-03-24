6beec46eedce64e8130f4f7ffd925b192c1cd6d9
zzsla <kangbs21026@naver.com>
2022-03-24 21:14:30 +0900

    수 정렬하기 3ver0.4

--- 10989.c
+++ 10989.c
@@ -1,38 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(void) //counting sort
 {
 	int N;
 	scanf("%i", &N);
 	if(!(1 <= N && N <= 10000000)) return 1;
-	int *n;
-	n = (int *)malloc(sizeof(int *) * N + 1);
-	int *cntnum;
-	cntnum = (int *)malloc(sizeof(int *) * 10000);
-	int tmp, tmp2;
-	for(int i = 0; i < N; i++)
-		scanf("%i", &n[i]);
+	int cntnum[10000] = {};
+	int tmp;
 	for(int i = 0; i < N; i++){
-		tmp = n[i];
+		scanf("%i", &tmp);
 		cntnum[tmp]++;
 	}
-	cntnum[1] += -1;
-	for(int i = 1; i < 10000; i++)
-		cntnum[i] += cntnum[i-1];
-	int i = N - 1;
-	tmp = n[cntnum[n[0]]];
-	n[cntnum[n[0]]] = n[0];
-	cntnum[n[0]]--;
-	while(i != 0){
-		tmp2 = n[cntnum[tmp]];
-		n[cntnum[tmp]] = tmp;
-		cntnum[tmp]--;
-		i--;
-		tmp = tmp2;
-	}
-	for(int i = 0; i < N; i++)
-		printf("%i\n",n[i]);
+	for(int i = 0; i < 10000; i++)
+		while(cntnum[i] != 0){
+			printf("%i\n",i);
+			cntnum[i]--;
+		}
 
 	return 0;
 }
