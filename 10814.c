89b70af0a7ee3ab8dff846c34ccb4a86dc1e1ca6
zzsla <kangbs21026@naver.com>
2022-03-28 21:14:30 +0900

    나이순 정렬ver0.1

--- 10814.c
+++ 10814.c
@@ -1,31 +1,36 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
 int main(void)
 {
 	int N;
+	int **age;
+	age = (int **)malloc(sizeof(int*) * 101);
+	for(int i = 0; i < 101; i++){
+		age[i] = (int *)malloc(sizeof(int) * 100000);
+	}
 	scanf("%i", &N);
 	if(!(1 <= N && N <= 100000)) return 1;
-	int age[N];
-	char name[N][101];
-	int tmp;
-	char tamp[101];
-	for(int i = 0; i < N; i++)
-		scanf("%i %s",&age[i], name[i]);
-	for(int num = N - 1; num > 0; num--) // Bubble Sort
-		for(int i = 0; i < num; i++){
-			int j = i + 1;
-			if(age[i] > age[j]){
-				tmp = age[i];
-				age[i] = age[j];
-				age[j] = tmp;
-				strcpy(tamp, name[i]);
-				strcpy(name[i], name[j]);
-				strcpy(name[j], tamp);
-			}
+	char name[N+1][101];
+	int count[101] = {};
+	int tmp = 0;
+	for(int i = 1; i <= N; i++){
+		scanf("%i %s", &tmp, name[i]);
+		int j = 0;
+		age[tmp][count[tmp]] = i;
+		count[tmp]++;
+	}
+	for(int i = 0; i < 101; i++){
+		int j = 0;
+		while(age[i][j] != 0){
+			tmp = age[i][j];
+			printf("%i %s\n", i, name[tmp]);
+			j++;
 		}
-	for(int i = 0; i < N; i++)
-		printf("%i %s\n", age[i], name[i]);
+	}
+	for(int i = 0; i < 101; i++)
+		free(age[i]);
+	free(age);
 
 	return 0;
 }
