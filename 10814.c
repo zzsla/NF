d2c8950f586191e5337bf18e9df681e8b64c3b17
zzsla <kangbs21026@naver.com>
2022-03-29 13:11:15 +0900

    나이순 정렬ver0.2

--- 10814.c
+++ 10814.c
@@ -1,36 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+typedef struct Person
+{
+	char name[101];
+	int age;
+}
+Person;
 
 int main(void)
 {
 	int N;
-	int **age;
-	age = (int **)malloc(sizeof(int*) * 101);
-	for(int i = 0; i < 101; i++){
-		age[i] = (int *)malloc(sizeof(int) * 100000);
-	}
+	
 	scanf("%i", &N);
 	if(!(1 <= N && N <= 100000)) return 1;
-	char name[N+1][101];
-	int count[101] = {};
-	int tmp = 0;
-	for(int i = 1; i <= N; i++){
-		scanf("%i %s", &tmp, name[i]);
-		int j = 0;
-		age[tmp][count[tmp]] = i;
-		count[tmp]++;
-	}
-	for(int i = 0; i < 101; i++){
-		int j = 0;
-		while(age[i][j] != 0){
-			tmp = age[i][j];
-			printf("%i %s\n", i, name[tmp]);
-			j++;
-		}
-	}
-	for(int i = 0; i < 101; i++)
-		free(age[i]);
-	free(age);
+	Person n[N];
+	for(int i = 0; i < N; i++)
+		scanf("%i %s", &n[i].age, n[i].name);
+	for(int i = 1; i < 201; i++)
+		for(int j = 0;j < N;j++)
+			if(n[j].age == i)
+				printf("%i %s\n",n[j].age, n[j].name);
 
 	return 0;
 }
