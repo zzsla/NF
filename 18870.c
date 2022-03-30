ba1f9897a984249389e4b80a6b37022a03dc4993
zzsla <kangbs21026@naver.com>
2022-03-30 22:27:38 +0900

    좌표 압축ver0.1

--- 18870.c
+++ 18870.c
@@ -1,32 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-typedef struct X
+int compare(const void *a, const void *b)
 {
-	int valve;
-	unsigned int result;
-}X;
-
+	int num1 = *(int *)a;
+	int num2 = *(int *)b;
+	if(num1 > num2) return 1;
+	if(num1 < num2) return -1;
+	return 0;
+}
 int main(void)
 {
 	int N;
 	scanf("%i", &N);
 	if(!(1 <= N && N <= 1000000)) return 1;
-
-	X n[N];
+	int n[N];
+	unsigned int check[N];
+	int result[2000000001];
+	unsigned int lastnum = 2000000001;
+	int count = 1;
 	for(int i = 0; i < N; i++){
-		scanf("%i", &n[i].valve);
-		n[i].result = 0;
+		scanf("%i", &n[i]);
+		check[i] = n[i] + 1000000000;
 	}
-	for(int i = 0; i < N -1; i++)
-		for(int j = i + 1; j < N; j++){
-			if(n[i].valve > n[j].valve)
-				n[i].result++;
-			else if(n[i].valve < n[j].valve)
-				n[j].result++;
+	qsort(check, sizeof(check)/sizeof(int), sizeof(int), compare);
+	for(int i = 0; i < N; i++)
+		if(!(lastnum == check[i])){
+			result[check[i]] = count;
+			count++;
+			lastnum = check[i];
 		}
 
 	for(int i = 0; i < N; i++)
-		printf("%i ", n[i].result);
+		printf("%i ", result[n[i]+1000000000] - 1);
 
 	return 0;
 }
